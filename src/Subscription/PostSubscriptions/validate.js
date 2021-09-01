ee4cb027c21194c481dca71e5b3448cb7783ff66
devendra-jit <85719346+sujit-bizcloudexperts@users.noreply.github.com>
2021-09-01 21:21:27 +0530

    fix test cases issues

---
+++ src/Subscription/PostSubscriptions/validate.js
@@ -0,0 +1,54 @@
+const Joi = require('joi');
+const get = require('lodash.get');
+const { handleError } = require('../../shared/utils/responses');
+
+const custom = Joi.extend((joi) => {
+    return {
+        type: 'object',
+        base: joi.object(),
+        coerce(value, schema) {
+
+            try {
+                return { value: JSON.parse(value) };
+            }
+            catch (err) {
+                return err;
+            }
+        }
+    };
+});
+
+const schema = Joi.object({
+    body: custom.object({
+        EventType: Joi.string().required(),
+        Preference: Joi.string().required(),
+        Endpoint: Joi.string()
+          .pattern(
+            new RegExp(
+              "^(https?:\\/\\/)?" +
+                "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|" +
+                "((\\d{1,3}\\.){3}\\d{1,3}))" +
+                "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" +
+                "(\\?[;&a-z\\d%_.~+=-]*)?" +
+                "(\\#[-a-z\\d_]*)?$",
+              "i"
+            )
+          )
+          .required(),
+        SharedSecret: Joi.string().required(),
+    }),
+    headers: Joi.object().keys({
+        "x-api-key": Joi.string().required()
+    }).unknown(true)
+}).unknown(true)
+
+async function validate(event) {
+    try {
+        event = await schema.validateAsync(event);
+    } catch (e) {
+        return handleError(1001, e, get(e, 'details[0].message', null));
+    }
+    return event;
+}
+
+module.exports = validate;
