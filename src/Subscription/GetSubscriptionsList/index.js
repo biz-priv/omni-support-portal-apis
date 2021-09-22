953ebdeb8f8c85924d4bc7c4a023372a9a4c055f
sujit <85719346+sujit-bizcloudexperts@users.noreply.github.com>
2021-09-22 18:12:18 +0530

    update pagination logic in webhooks subscription list api.

--- src/Subscription/GetSubscriptionsList/index.js
+++ src/Subscription/GetSubscriptionsList/index.js
@@ -1,4 +1,4 @@
-const { send_response } = require('../../shared/utils/responses');
+const { send_response, handleError } = require('../../shared/utils/responses');
 const validate = require('./validate');
 const Dynamo = require('../../shared/dynamo/db');
 const { get } = require('lodash');
@@ -10,12 +10,22 @@ module.exports.handler = async (event) => {
     console.info("Event: ", JSON.stringify(event));
     event = await validate(event);
     if (!event.code) {
-        let startKey = { Customer_Id: get(event, 'queryStringParameters.startkey'), Event_Type: get(event, 'queryStringParameters.endkey') };
-        let getItemResult, totalCount
+        let totalCount = 0;
+        let page = get(event, 'queryStringParameters.page')
+        let size = get(event, 'queryStringParameters.size')
         try {
-            startKey = (startKey.Customer_Id == null || startKey.Customer_Id == 0) ? null : startKey;
-            [getItemResult, totalCount] = await Promise.all([Dynamo.fetchAllItems(EVENT_PREFERENCE, get(event, 'queryStringParameters.size'), startKey), Dynamo.getAllItems(EVENT_PREFERENCE)]);
-            return await getResponse(getItemResult, totalCount.Count, startKey, get(event, 'queryStringParameters.page'), get(event, 'queryStringParameters.size'), event);
+            const fullRecords = await Dynamo.scanTableData(EVENT_PREFERENCE);
+            if (fullRecords.length) {
+                totalCount = fullRecords.length;
+                if (page > Math.ceil(totalCount / size)) {
+                    return send_response(400, handleError(1018))
+                }
+                return await getResponse(fullRecords, totalCount, page, size, event);
+            } else {
+                const error = handleError(1009);
+                console.error("Error: ", JSON.stringify(error));
+                return send_response(error.httpStatus, error)
+            }
         } catch (e) {
             console.error("Error: ", JSON.stringify(e));
             return send_response(e.httpStatus, e);
@@ -27,39 +37,16 @@ module.exports.handler = async (event) => {
 
 }
 
-async function getResponse(results, count, startkey, page, size, event) {
-    let resp = {}
-    resp["Subscription"] = get(results, 'Items', []);
-
-    let elementCount = resp['Subscription'].length;
-    let deployStage = get(event, 'requestContext.stage', 'devint');
-    let lastCustId = 0;
-    let lastKey = 0;
-
-    if (get(results, 'LastEvaluatedKey', null)) {
-        lastCustId = get(results, 'LastEvaluatedKey.Customer_Id');
-        lastKey = get(results, 'LastEvaluatedKey.Event_Type')
-        var LastEvaluatedkey = "&startkey=" + lastCustId + "&endkey=" + lastKey;
-    }
-
-    let prevLinkStartKey = 0;
-    let prevLinkEndKey = 0;
-    if (startkey !== null) {
-        prevLinkStartKey = startkey.Customer_Id;
-        prevLinkEndKey = startkey.Event_Type;
-    }
-
-    let host = get(event, 'headers.Host', null) + "/" + deployStage;
+async function getResponse(results, count, page, size, event) {
+    let selfPageLink = "N/A";
+    let host = "https://" + get(event, 'headers.Host', null) + "/" + get(event, 'requestContext.stage', 'devint');
     let path = get(event, 'path', null);
-    let prevLink = host + path + "?page=" + page + "&size=" +
-        size + "&startkey=" + prevLinkStartKey + "&endkey=" + prevLinkEndKey;
-
-    var response = await pagination.createPagination(resp, host, path + "?page=", page, size, elementCount, LastEvaluatedkey, count, prevLink);
-
-    if (lastCustId !== 0) {
-        response.Page["StartKey"] = lastCustId;
-        response.Page["EndKey"] = lastKey;
-    }
+    selfPageLink = "?page=" + page + "&size=" +
+        size + "&startkey=" + get(event, 'queryStringParameters.startkey') + "&endkey=" + get(event, 'queryStringParameters.endkey');
+    let startkey = "Customer_Id"
+    let endkey = "Event_Type"
+    let responseArrayName = "Subscription"
+    var response = await pagination.createPagination(results, responseArrayName, startkey, endkey, host, path, page, size, count, selfPageLink);
     console.info("Response: ", JSON.stringify(response));
     return send_response(200, response);
 }
